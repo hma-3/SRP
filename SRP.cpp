79a8ec81939531ac4fbdf806826d4af0d01acf22
hma-3 <maria.gula0307@gmail.com>
2023-12-16 22:04:22 +0200

    Initial commit

---
+++ SRP.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+class User {
+    string name;
+    string email;
+public:
+    User(string name, string email)
+        : name(name), email(email) {}
+
+    string getName() {
+        return name;
+    }
+
+    string getEmail() {
+        return email;
+    }
+};
+
+class UserPrinter {
+public:
+    void printUserDetails(User user) {
+        cout << "User Details:\n";
+        cout << "Name: " << user.getName() << endl;
+        cout << "Email: " << user.getEmail() << endl;
+    }
+};
+
+class UserManager {
+public:
+    void addUser(string name, string email) {
+        User newUser(name, email);
+    }
+};
+
+int main() {
+    return 0;
+}
